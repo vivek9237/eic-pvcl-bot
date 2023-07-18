d772788c2e6d38e13efc74bafbf3e09c9745d020
Vivek.Mohanty <vivek.ku.mohanty@gmail.com>
2023-07-18 22:23:52 +0530

    added data analyzer and feaure based testing

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ features = features_csv.split(config_delimiter)
 driver = webdriver.Chrome()
 driver.maximize_window()
 
+master_report = {}
+
 def eic_login(url,username,password):
     driver.get(url)
     usernameField = driver.find_element("xpath",'//*[@id="username"]') #id = username
@@ -72,65 +74,103 @@ def perform_save_connection():
             retry = retry - 1
             continue
     return connection_status.strip()
-    
-def write_result_xlsx(result_dict):
-    workbook = xlsxwriter.Workbook(report_name)
-    try:
-        worksheet = workbook.add_worksheet()
-        worksheet.write('A1', 'Connection Name')
-        worksheet.write('B1', 'Test Connection Result')
-        i=2
-        for connection_name in result_dict:
-            worksheet.write('A'+str(i), connection_name)
-            worksheet.write('B'+str(i), result_dict[connection_name])
-            i = i + 1
-    except:
-        print("Error saving the result into the report!")
-    finally:
-        workbook.close()
+
 
 def open_data_analyzer_page():
     driver.get(url+data_analyzer_page_url)
     
 def run_data_analyzer_query(query):
     queryField = driver.find_element("xpath",'//*[@id="queryArea"]')
+    queryField.clear()
     queryField.send_keys(query)
-    querySubmitButton = driver.find_element("xpath",'//*[@id="querySubmit"]')
+    time.sleep(5)
+    querySubmitButton = driver.find_element("xpath",'//*[@id="querySubmit"]') # add retry mechanism here with delay
     querySubmitButton.click()
     time.sleep(5)
+    results = driver.find_element("xpath",'//*[@id="resultTable"]/tbody/tr/td') # add retry mechanism here with delay
+    nextButton = driver.find_element(By.LINK_TEXT, "Next")
+    nextButton.click()
+    return results.text
     
 def check_data_analyzer():
+    master_query_list = []
+    data_analyzer_report_header = ["Query","Result"]
+    master_query_list.append(data_analyzer_report_header)
     try:
         open_data_analyzer_page()
         driver.save_screenshot("results/data_analyzer/before_running_query.png")
-        run_data_analyzer_query("select * from users where username='admin'")
-        driver.save_screenshot("results/data_analyzer/after_running_query.png")
-    except:
+        query_csv = config['DATA_ANALYZER']['QUERY']
+        query_list = query_csv.split(";")
+        count = 1
+        for query_string in query_list:
+            if (query_string.strip() != ""):
+                try:
+                    result = run_data_analyzer_query(query_string.strip())
+                    temp_result_list = [query_string.strip(),str(result)]
+                    master_query_list.append(temp_result_list)
+                    print(query_string.strip() +" = "+str(result))
+                except Exception as error:
+                    temp_result_list = [query_string.strip(),"**Unknown Exception**"]
+                    master_query_list.append(temp_result_list)
+                    print(query_string.strip() +" = "+str(error))
+                finally:
+                    driver.save_screenshot("results/data_analyzer/after_running_query"+str(count)+".png")
+                    count = count + 1
+    except Exception as error1:
         driver.save_screenshot("results/data_analyzer/unknown_error.png")
+        print(error1)
+    master_report["DATA_ANALYZER"] = master_query_list
 
 def check_connections():
-    test_connection_result_dict = {}
+    master_connection_list = []
+    connection_report_header = ["Connection Name","Test Result"]
+    master_connection_list.append(connection_report_header)
     for connection_name in connections:
         try:
             open_connection_page()
             open_connection(connection_name)
             connection_status = perform_save_connection()
-            test_connection_result_dict[connection_name] = connection_status
+            temp_result_list = [connection_name,connection_status]
+            master_connection_list.append(temp_result_list)
             print(connection_name+" - "+connection_status)
             driver.save_screenshot("results/connections/tested/"+connection_name+".png")
         except:
             driver.save_screenshot("results/connections/intervention_required/"+connection_name+".png")
-            test_connection_result_dict[connection_name] = "**Manual intervention required**"
+            temp_result_list = [connection_name,"**Manual intervention required**"]
+            master_connection_list.append(temp_result_list)
             print("Some error occured for Connection - "+connection_name)
-    write_result_xlsx(test_connection_result_dict)
+    master_report["CONNECTION"] = master_connection_list
 
 def start_feature_based_testing():
     for feature_name in features:
-        if(feature_name=="CONNECTIONS"):
-            check_connections()
-        elif(feature_name=="DATA_ANALYZER"):
-            check_data_analyzer()
-    
+        feature_name = feature_name.strip()
+        try:
+            print("\nTesting started for - "+feature_name)
+            if(feature_name=="CONNECTIONS"):
+                check_connections()
+            elif(feature_name=="DATA_ANALYZER"):
+                check_data_analyzer()
+            else:
+                print("Unsupported FEATURE - "+feature_name)
+        except:
+            print("Error while testing - "+feature_name)  
+        
+def prepare_master_report(master_dict):
+    xlsxFile = xlsxwriter.Workbook("results/"+report_name)
+    for sheet_name in master_dict:
+        temp_sheet = xlsxFile.add_worksheet(sheet_name)
+        row = 0
+        for record in master_dict[sheet_name]:
+            column = 0
+            for data in record:
+                temp_sheet.write(row,column,data)
+                column = column + 1
+            row = row + 1
+    xlsxFile.close()
+
+create_result_dirs():
+    print("Creating result directories <under construction>")
+
 def print_eta():
     print("ETA : "+"<under construction>")
 
@@ -146,8 +186,10 @@ def print_eta():
 
 def main():
     print_eta()
+    create_result_dirs()
     eic_login(url,username,password)
     start_feature_based_testing()
+    prepare_master_report(master_report)
 
 if __name__ == "__main__":
     main()
